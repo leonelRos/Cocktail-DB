52bb983c5785d22f949238e089080c0f69cd1063
leonelros <rleonel426@gmail.com>
2020-09-09 15:08:49 -0700

    adding new files

--- models/glassware.js
+++ models/glassware.js
@@ -1,11 +1,14 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-var glasswareSchema = new Schema({
+var glasswareSchema = new Schema(
+  {
     type_of_glass: { type: String, required: true, unique: true },
-    material: String
-}, {
-    timestamps: true
-});
+    material: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Glassware", glasswareSchema);
