2cd8fab2cdf8e63ed8bfdcb21b6330190fc2eda4
leonelros <rleonel426@gmail.com>
2020-09-15 21:27:32 -0700

    refactoring

--- controllers/cocktails.js
+++ controllers/cocktails.js
@@ -1,46 +1,50 @@
-var Cocktail = require('../models/cocktail');
-var Glassware = require('../models/glassware');
+var Cocktail = require("../models/cocktail");
+var Glassware = require("../models/glassware");
 
 module.exports = {
-    index,
-    show,
-    new: newCocktail,
-    create
+  index,
+  show,
+  new: newCocktail,
+  create,
 };
 
 function index(req, res) {
-    Cocktail.find({}, function (err, cocktails) {
-        res.render('cocktails/index', {
-            title: 'Cocktails List',
-            cocktails
-        })
+  Cocktail.find({}, (err, cocktails) => {
+    res.render("cocktails/index", {
+      title: "Cocktails List",
+      cocktails,
     });
-};
+  });
+}
 
 function show(req, res) {
-    Cocktail.findById(req.params.id).populate("glass").exec(function (err, cocktail) {
-        Glassware.find({ _id: { $nin: cocktail.glass } })
-            .exec(function (err, glassware) {
-            console.log(glassware)
-            res.render('cocktails/show', {
-                title: 'Cocktail Details',
-                cocktail, glassware
-            })
-        })
+  Cocktail.findById(req.params.id)
+    .populate("glass")
+    .exec((err, cocktail) => {
+      Glassware.find({ _id: { $nin: cocktail.glass } }).exec(
+        (err, glassware) => {
+          console.log(glassware);
+          res.render("cocktails/show", {
+            title: "Cocktail Details",
+            cocktail,
+            glassware,
+          });
+        }
+      );
     });
-};
+}
 
 function create(req, res) {
-    var cocktail = new Cocktail(req.body);
-    cocktail.save(function (err) {
-        if (err) return res.render('cocktails/new');
-        console.log(cocktail)
-        res.redirect('/cocktails')
-    })
+  var cocktail = new Cocktail(req.body);
+  cocktail.save(function (err) {
+    if (err) return res.render("cocktails/new");
+    console.log(cocktail);
+    res.redirect("/cocktails");
+  });
 }
 
 function newCocktail(req, res) {
-    res.render('cocktails/new', {
-        title: "Add Cocktail"
-    })
+  res.render("cocktails/new", {
+    title: "Add Cocktail",
+  });
 }
--- controllers/glasswares.js
+++ controllers/glasswares.js
@@ -1,32 +1,32 @@
-var Glassware = require('../models/glassware');
-var Cocktail = require('../models/cocktail')
+var Glassware = require("../models/glassware");
+var Cocktail = require("../models/cocktail");
 
 module.exports = {
-    new: newGlassware,
-    create,
-    addToGlass
+  new: newGlassware,
+  create,
+  addToGlass,
 };
 
 function addToGlass(req, res) {
-    Cocktail.findById(req.params.id, function(err, cocktail) {
-        cocktail.glass.push(req.body.glasswareId);
-        cocktail.save(function(err) {
-            res.redirect(`/cocktails/${cocktail._id}`);
-        });
+  Cocktail.findById(req.params.id, (err, cocktail) => {
+    cocktail.glass.push(req.body.glasswareId);
+    cocktail.save((err) => {
+      res.redirect(`/cocktails/${cocktail._id}`);
     });
+  });
 }
 
 function create(req, res) {
-    Glassware.create(req.body, function(err, glassware) {
-        res.redirect('/glasswares/new')
-    })
+  Glassware.create(req.body, (err, glassware) => {
+    res.redirect("/glasswares/new");
+  });
 }
 
 function newGlassware(req, res) {
-    Glassware.find({}, function(err, glassware) {
-        res.render('glasswares/new', {
-            title: 'Add Glassware',
-            glassware
-        });
+  Glassware.find({}, (err, glassware) => {
+    res.render("glasswares/new", {
+      title: "Add Glassware",
+      glassware,
     });
+  });
 }
