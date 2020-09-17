25eb3236e64b8c135865349335c1c912d553f3d5
leonelros <rleonel426@gmail.com>
2020-09-17 16:10:44 -0700

    showing raw JSON data

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
-const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const request = require("request");
-const rootURL = "https://thecocktaildb.com/api/json/v2/9973533/search.php?s";
+const rootURL = "https://thecocktaildb.com/api/json/v2/9973533/search.php?s=";
 console.log(rootURL);
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
   res.render("index", {
     title: "Cocktail-DB",
+    userDrink: null,
+  });
+});
+
+router.post("/", (req, res, next) => {
+  request(rootURL + req.body.drinks, (err, response, body) => {
+    // var userDrink = JSON.parse(body);
+    console.log(req.body.drinks);
+    res.render("index", { userDrink: body });
   });
 });
 
