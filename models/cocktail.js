4ef8a9465cf836871c250ae0923a43553e8f3880
leonelros <rleonel426@gmail.com>
2020-09-24 13:34:08 -0700

    adding delete method

--- models/cocktail.js
+++ models/cocktail.js
@@ -1,57 +1,61 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-var reviewCocktails = new Schema({
+var reviewCocktails = new Schema(
+  {
     content: {
-        type: String,
-       
+      type: String,
     },
-    
-    rating: {
-        type: Number,
-        min: 1,
-        max: 5,
-        default: 5
-    }
-},
-{
-    timestamps: true
-})
 
-var cocktailSchema = new Schema({
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 5,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+var cocktailSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: String,
     ingredient1: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     ingredient2: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     ingredient3: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     ingredient4: {
-        type: String,
-        // required: true
+      type: String,
+      // required: true
     },
     ingredient5: {
-        type: String,
-        // required: true
+      type: String,
+      // required: true
     },
     preparation: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     reviews: [reviewCocktails],
-    glass: [{type: Schema.Types.ObjectId, ref: 'Glassware'}]
-}, {
-    timestamps: true
-});
+    glass: [{ type: Schema.Types.ObjectId, ref: "Glassware" }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Cocktail', cocktailSchema);
+module.exports = mongoose.model("Cocktail", cocktailSchema);
